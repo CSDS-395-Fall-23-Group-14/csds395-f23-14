785c744fdbc0828162f13d69a72d707aa80f64a9
Soren Schultz <sschultz@magicmusicapp.com>
2023-09-21 12:55:05 -0400

    add hover behavior

--- src/components/barChart/barChart.js
+++ src/components/barChart/barChart.js
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from "react";
+import { useState, useEffect, useRef } from "react";
 import {
   axisBottom,
   axisLeft,
@@ -33,50 +33,87 @@ function YAxis({ scale }) {
   return <g ref={ref} />;
 }
 
-function Bars({ data, xAxisHeight, scaleX, scaleY }) {
+function Bars({ data, xAxisHeight, scaleX, scaleY, hover, unHover, hoveredBar }) {
   return (
     <>
-      {data.map(({ chg_percent, name }) =>
-        <rect
-          key={`bar-${name}`}
-          x={scaleX(name)}
-          y={0 <= chg_percent ? scaleY(chg_percent) : xAxisHeight}
-          width={scaleX.bandwidth()}
-          height={(xAxisHeight - scaleY(Math.abs(chg_percent)))}
-          fill={0 <= chg_percent ? 'green' : 'red'}
-        />
-      )}
+      {
+        data.map((datum) =>
+          <rect
+            key={`bar-${datum.name}`}
+            x={scaleX(datum.name)}
+            y={0 <= datum.chg_percent ? scaleY(datum.chg_percent) : xAxisHeight}
+            width={scaleX.bandwidth()}
+            height={(xAxisHeight - scaleY(Math.abs(datum.chg_percent)))}
+            fill={0 <= datum.chg_percent ? 'green' : 'red'}
+            style={{'stroke': 'black', 'stroke-width': (datum === hoveredBar ? 2 : 0)}}
+            onMouseOver={() => hover(datum)}
+            onMouseOut={() => unHover()}
+          />
+        )
+      }
+      
+      {
+        hoveredBar ?
+        <ToolTip
+          datum={hoveredBar}
+          scaleX={scaleX}
+          scaleY={scaleY}
+        /> : null
+      }
     </>
   );
 }
 
+function ToolTip({ datum, scaleX, scaleY }) {
+  console.log(datum);
+  return <text x="20" y="20">
+    {datum.name}
+  </text>
+}
+
 function BarChart({ data }) {
+  const [ hoveredBar, setHoveredBar ] = useState(null);
+  
+  const dataSorted = data.sort((a, b) => b.chg_percent - a.chg_percent)
+  
   const margin = { top: 10, right: 0, bottom: 20, left: 30 };
   const width = 500 - margin.left - margin.right;
   const height = 300 - margin.top - margin.bottom;
 	
-	const chgs = data.map(({ chg_percent }) => chg_percent);
-  const minChg = Math.min(...chgs, 0);
-  const maxChg = Math.max(...chgs, 0);
+	const chgs = dataSorted.map(({ chg_percent }) => chg_percent);
+  const minChg = Math.min(...chgs, -1.25);
+  const maxChg = Math.max(...chgs, 1.25);
 
   const scaleX = scaleBand()
-    .domain(data.map(({ name }) => name))
+    .domain(dataSorted.map(({ name }) => name))
     .range([0, width])
     .padding(0.5);
+    
   const scaleY = scaleLinear()
-    .domain([minChg * 1.25, maxChg * 1.25])
+    .domain([minChg - 0.25, maxChg + 0.25])
     .range([height, 0]);
   
   const xAxisHeight = height * maxChg / (maxChg - minChg);
 
   return (
-    <svg viewBox='0 0 500 500'>
-      <g transform={`translate(${margin.left}, ${margin.top})`}>
-        <Bars data={data} xAxisHeight={xAxisHeight} scaleX={scaleX} scaleY={scaleY} />
-        <XAxis scale={scaleX} transform={`translate(0, ${xAxisHeight})`} />
-        <YAxis scale={scaleY} />
-      </g>
-    </svg>
+    <>
+      <svg viewBox='0 0 500 300'>
+        <g transform={`translate(${margin.left}, ${margin.top})`}>
+          <Bars
+            data={dataSorted}
+            xAxisHeight={xAxisHeight}
+            scaleX={scaleX}
+            scaleY={scaleY}
+            hover={(bar) => setHoveredBar(bar)}
+            unHover={() => setHoveredBar(null)}
+            hoveredBar={hoveredBar}
+          />
+          <XAxis scale={scaleX} transform={`translate(0, ${xAxisHeight})`} />
+          <YAxis scale={scaleY} />
+        </g>
+      </svg>
+      
+    </>
   );
 }
 
