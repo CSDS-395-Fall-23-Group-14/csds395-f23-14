30119cdc1216e2773ae693f0027a6aabaf1df3ea
sjs262 <sjs262@case.edu>
2023-10-22 15:01:41 -0400

    parametrize barchart

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,7 +7,11 @@ import EnhancedTable from '../components/EnhancedTable/EnhancedTable';
 import EnhancedBarChart from '../components/EnhancedBarChart/EnhancedBarChart';
 
 import { DataContext } from '../context/DataContext';
-import { Skeleton } from '@mui/material';
+import {
+  Skeleton,
+  TextField,
+  MenuItem
+} from '@mui/material';
 
 /**
  * The Home component representing the main page of the application.
@@ -19,7 +23,8 @@ function Home() {
 	const { get25Stocks } = useContext(DataContext);
 	
 	const [data, setData] = useState([]);
-	const [isLoading, setIsLoading] = useState(true); // Potential for a loading animation
+	const [isLoading, setIsLoading] = useState(true);
+  const [barChartProperty, setBarChartProperty] = useState('currentprice');
 	
 	useEffect(() => {
 		const fetchData = async () => {
@@ -49,6 +54,11 @@ function Home() {
     'left', 'center', 'center', 'center', 'center',
     'center', 'center', 'center', 'center', 'left',
   ]
+  
+  const types = [
+    'string', 'string', 'string', 'number', 'number',
+    'number', 'number', 'string', 'number', 'string',
+  ]
 	
 	const columns = fields.map((_, i) => ({
 			field: fields[i],
@@ -56,6 +66,7 @@ function Home() {
       flex: widths[i],
       align: aligns[i],
       headerAlign: aligns[i],
+      type: types[i]
 		})
   );
   
@@ -73,6 +84,7 @@ function Home() {
             className='barCharts'
             m='auto'
             sx={{py: "1%"}}
+            width={1200}
           >
             {isLoading ?
               <Skeleton
@@ -82,11 +94,29 @@ function Home() {
               /> :
               <EnhancedBarChart
                 data={data}
-                property='currentprice'
+                property={barChartProperty}
                 width={1200}
                 height={300}
               />
             }
+            <TextField
+              sx={{ my: '0.5%' }}
+              fullWidth
+              select
+              defaultValue={columns[3]['field']}
+              onChange={(e) => setBarChartProperty(e.target.value)}
+            >
+              {
+                columns.filter((_, i) => types[i] === 'number').map((e) =>
+                  <MenuItem
+                    key={e.field}
+                    value={e.field}
+                  >
+                    {e.headerName}
+                  </MenuItem>
+                )
+              }
+            </TextField>
           </Box>
           <Box
             sx={{borderTop: 1, py: "1%"}}
